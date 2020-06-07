d5c4415a30f4ac90872acdd38d60bdc748627a23
tarun tiwari <git config --global user.email taruntiwaritt1997@gmail.com>
2020-06-07 22:33:13 +0530

    Updates

---
+++ precache-manifest.c8d21644432ab9c2a1932f4d163c9743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d05560e404dfff4ef2939eff447c19",
+    "url": "/simple-react-crud/index.html"
+  },
+  {
+    "revision": "4399b580389f5cb66c98",
+    "url": "/simple-react-crud/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652416f7364a94537df0",
+    "url": "/simple-react-crud/static/js/2.c488c741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-crud/static/js/2.c488c741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4399b580389f5cb66c98",
+    "url": "/simple-react-crud/static/js/main.c14eeedb.chunk.js"
+  },
+  {
+    "revision": "9379666ad9899b2d3013",
+    "url": "/simple-react-crud/static/js/runtime-main.63d23e74.js"
+  }
+]);
